636d903e55733817c897d11117ae89467f787b39
Darshan <priyadarshanmanoharan@gmail.com>
2023-06-14 05:03:30 -0700

    React-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container text-center">
+        <h1 class=" display-4">Registration Form</h1>
+<label >Name</label>
+<input type="text" class="mx-5" placeholder="Name"></input>
+<br></br>
+<label>Phone</label>
+<input type="tel" class="mx-5" placeholder="Name"></input>
+
     </div>
   );
 }
